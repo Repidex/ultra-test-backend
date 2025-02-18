473c59f46d8abc28dbc37f65d2bd9ceff8b826dd
repidex <jitenparmar407@gmail.com>
2025-02-18 17:01:43 +0530

    - ip whitelist added

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+var morgan = require('morgan')
 const employeeRoutes = require("./src/routes/employee.route");
 
 const app = express();
 
 app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 
 mongoose
-  .connect("mongodb+srv://jitenprmar7:X2QYZRftPjxUBO2T@cluster0.glfcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb+srv://jitenprmar7:X2QYZRftPjxUBO2T@cluster0.glfcf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
